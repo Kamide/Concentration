7b28d735efb3de90a86f853dc8187d1afc84f638
Kangming Deng <dengkangming@gmail.com>
2020-09-30 16:21:44 -0400

    Update main.js

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Main extends Component {
   render() {
@@ -7,6 +8,14 @@ export default class Main extends Component {
         <header className="secondary">
           <h1 className="parallelogram">Concentration</h1>
         </header>
+        <p>
+          <strong>Concentration</strong>, also known as <strong>Memory</strong>, is a card game.
+          The objective is to find and match all card pairs.
+          You can play by yourself or team up with friends!
+        </p>
+        <p>
+          You can find a game room in the <Link to='/lobby'>lobby</Link> or you can create your own game room <Link to='/room'>here</Link>.
+        </p>
       </main>
     );
   }
